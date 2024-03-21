f17e5df125c71a8e502c55323184d38e96099a6e
manu-cj <carenjotje@gmail.com>
2024-03-21 22:08:46 +0100

    utilisation d'express

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/acceuil', (req, res) => {
+    res.status(200).sendFile("./views/acceuill.html", {root : __dirname});
+});
+
+app.get('/profil', (req, res) => {
+    res.status(200).sendFile("./views/profil.html", {root : __dirname});
+});
+
+app.get("/", (req, res) => {
+    res.status(300).redirect("/acceuil");
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile("./views/404.html", {root : __dirname});
+})
+
+app.listen(8001, () => {
+    console.log('En attente de requête sur le port 8001');
+})
+console.log('Erreur lors de la création du serveur sur le port 8001');
